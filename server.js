c569b1a8d0bb5a859b46729e43d129c6e552e1e0
tofu11 <87494814+tofu11@users.noreply.github.com>
2023-11-06 02:03:08 -0500

    added database

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const app = express()
 const port= 1234
 //get request using HTTPS
 app.use(express.static('public'))
+//stores data into the json file
+app.use(express.json)
+
 
 app.post('/', (req,res)=>{
-
-    res.status(200).send('testing')
+    const {id,question,options}=req.body
+    console.log(id,question,options)
+    res.sendStatus(200)
     //res.send('ur mom is gay')
 })
 app.listen(port,()=> console.log(`server has started on port: ${port}`))
